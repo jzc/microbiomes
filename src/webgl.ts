685c1172a88848ab720a7a506477b3b36ec173c6
jzc <justincai.lps@gmail.com>
2018-12-11 20:01:22 -0700

    gh-pages

--- src/webgl.ts
+++
@@ -1,8 +0,0 @@
-const canvas = <HTMLCanvasElement> document.getElementById("glcanvas");
-export const gl = canvas.getContext("webgl2")!;
-
-gl.clearDepth(1);
-gl.clearColor(135/255, 206/255, 250/255, 1);
-gl.enable(gl.DEPTH_TEST);
-gl.enable(gl.CULL_FACE);
-gl.cullFace(gl.BACK);
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mode: "development",
-    entry: "./build/main.js",
-    devtool: 'inline-source-map',
-    output: {
-        filename: "main.js",
-        publicPath: "dist"
-    }
-}
